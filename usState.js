3cb9c6c2ce4ab4fc414fba1608d3a1de35653060
Michael Hester <michaelvhester@gmail.com>
2019-04-30 15:54:03 -0400

    US State V1

--- usState.js
+++ usState.js
@@ -1,4 +1,4 @@
-//---->GLOBAL VARIABLES FOR US COUNTY<-----//
+//---->GLOBAL VARIABLES FOR US STATE<-----//
 const container = d3.select('body').append('div').attr('class', 'map-container');
     
 let svg,
@@ -10,8 +10,7 @@ let outerW,
     w,
     h;
 
-let counties,
-    countiesMesh,
+let states,
     projection,
     path;
 
@@ -27,13 +26,12 @@ function build() {
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
     g.selectAll('path')
-        .data(counties.features)
+        .data(states.features)
         .enter()
         .append('path')
         .attr('d', path)
         .attr('fill', 'white')
         .attr('stroke', 'black')
-        .on('mouseover', d => console.log('hi'))
 
 }
 
@@ -50,38 +48,39 @@ function setup() {
 
   w = outerW - margin.left - margin.right;
   h = outerH - margin.top - margin.bottom;
-
-  projection = d3.geoAlbersUsa()
-    .scale(1)
-    .translate([0, 0]);
   
   path = d3.geoPath()
       .projection(projection);
 
-  const parameters = getProjectionParameters();
+    projection = d3.geoAlbersUsa()
+      .scale(1)
+      .translate([0, 0]);
 
-  projection
-      .scale(parameters[0])
-      .translate(parameters[1]);
+  getProjectionParameters();
 
   build();
 }
 
 function init() {
-    d3.loadData('../assets/data/usCounty.json', function(err, res){
+    d3.loadData('../assets/data/usState.json', function(err, res){
         const topo = res[0];
-        counties = topojson.feature(topo, topo.objects.counties);
-        countiesMesh = topojson.mesh(topo, topo.objects.counties);
+        states = topojson.feature(topo, topo.objects.states);
         setup();
     })
 }
 
 function getProjectionParameters() {
-    b = path.bounds(counties),
+    projection = d3.geoAlbersUsa()
+        .scale(1)
+        .translate([0, 0]);
+
+    b = path.bounds(states),
     s = .95 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h),
     t = [(w - s * (b[1][0] + b[0][0])) / 2, (h - s * (b[1][1] + b[0][1])) / 2];
 
-    return [s, t];
+    projection
+        .scale(s)
+        .translate(t);
 }
 
 export default {init};
